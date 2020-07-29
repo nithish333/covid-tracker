06a7f77bca11e57c8870b98c9c9f4ae3ea809f51
nithish333 <121003145@sastra.ac.in>
2020-07-29 16:55:46 +0530

    Updates

---
+++ precache-manifest.6927c8e10979aa0069220a59131ec169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8c7e54bc89e3e9e7782252994e6914",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "62429ef330ecfa155fa0",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1c35f9cff4d2ccb81ee9",
+    "url": "/covid-tracker/static/css/main.d74c43f7.chunk.css"
+  },
+  {
+    "revision": "62429ef330ecfa155fa0",
+    "url": "/covid-tracker/static/js/2.d5a7220b.chunk.js"
+  },
+  {
+    "revision": "9946d09cf1d000b4a165711edf4119ce",
+    "url": "/covid-tracker/static/js/2.d5a7220b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c35f9cff4d2ccb81ee9",
+    "url": "/covid-tracker/static/js/main.a0cae108.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
