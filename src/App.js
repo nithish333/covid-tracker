eb38fbd03414984f8f047fb1926d6880d5c0b71c
nithish333 <121003145@sastra.ac.in>
2020-07-29 10:30:05 +0530

    Changed the api again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 import Cards from "./components/Cards/Cards";
 import SearchBar from "./components/SearchBar/SearchBar";
-// import Map from "./components/Map/Map";
+import Map from "./components/Map/Map";
 import "leaflet/dist/leaflet.css";
 import Chart from "./components/Chart/Chart";
 class App extends React.Component {
@@ -22,53 +22,77 @@ class App extends React.Component {
         lng: -40.4796,
       },
       mapZoom: 3,
+      countries: [],
     };
   }
   async componentDidMount() {
-    const responseFromUrl = await fetch("https://api.covid19api.com/summary");
-    const data = await responseFromUrl.json();
-    // console.log(data.Global);
-    const confirmed = data.Global.TotalConfirmed;
-    const recovered = data.Global.TotalRecovered;
-    const deaths = data.Global.TotalDeaths;
-    const lastUpdate = data.Date;
-    // console.log(data.confirmed.value);
-    // console.log(data);
-    this.setState({
-      confirmed,
-      recovered,
-      deaths,
-      lastUpdate,
-    });
-  }
-  handleCountryChange = async (event) => {
-    if (event.target.value !== "Global") {
-      const getCountryResponse = await fetch(
-        `https://api.covid19api.com/live/country/${event.target.value}/status/confirmed
-        `
+    try {
+      const responseFromUrl = await fetch("https://disease.sh/v3/covid-19/all");
+      const data = await responseFromUrl.json();
+      // console.log(data);
+      const countriesResponse = await fetch(
+        "https://disease.sh/v3/covid-19/countries"
       );
-      const getCountryData = await getCountryResponse.json();
-      // console.log(getCountryData);
-      const confirmed = getCountryData[0].Confirmed;
-      const recovered = getCountryData[0].Recovered;
-      const deaths = getCountryData[0].Deaths;
+      const countryData = await countriesResponse.json();
+
+      const countries = countryData.map((country) => country.country);
+      // console.log(countries);
+
+      const confirmed = data.cases;
+      const recovered = data.recovered;
+      const deaths = data.deaths;
+      const lastUpdate = data.Date;
       this.setState({
         confirmed,
         recovered,
         deaths,
+        lastUpdate,
+        mapCenter: {
+          lat: 34.80476,
+          lng: -40.4796,
+        },
+        mapZoom: 3,
+        countries: countries,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  handleCountryChange = async (event) => {
+    console.log(event.target.value);
+    if (event.target.value !== "Global") {
+      const getCountryResponse = await fetch(
+        `https://disease.sh/v3/covid-19/countries/${event.target.value}
+        `
+      );
+      const getCountryData = await getCountryResponse.json();
+      // console.log(getCountryData);
+      const confirmed = getCountryData.cases;
+      const recovered = getCountryData.recovered;
+      const deaths = getCountryData.deaths;
+      this.setState({
+        confirmed,
+        recovered,
+        deaths,
+        mapCenter: {
+          lat: getCountryData.countryInfo.lat,
+          lng: getCountryData.countryInfo.long,
+        },
+        mapZoom: 4,
       });
     } else {
       this.componentDidMount();
     }
   };
   render() {
-    const { confirmed, recovered, deaths, lastUpdate } = this.state;
+    const { confirmed, recovered, deaths, lastUpdate, countries } = this.state;
     return (
       <div className="App">
         <h2 className="Heading">covid-19 tracker</h2>
 
         <SearchBar
           handleCountryChange={(event) => this.handleCountryChange(event)}
+          countries={countries}
         />
         <Cards
           confirmed={confirmed}
@@ -76,8 +100,8 @@ class App extends React.Component {
           deaths={deaths}
           lastUpdate={lastUpdate}
         />
+        <Map center={this.state.mapCenter} zoom={this.state.mapZoom} />
         <Chart confirmed={confirmed} recovered={recovered} deaths={deaths} />
-        {/* <Map center={this.state.mapCenter} zoom={this.state.mapZoom} /> */}
       </div>
     );
   }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,41 +2,40 @@ import React from "react";
 import "./SearchBar.css";
 import Countries from "../Countries/Countries";
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      countries: [],
-    };
-  }
-  async componentDidMount() {
-    try {
-      const countriesResponse = await fetch(
-        "https://api.covid19api.com/summary "
-      );
-      const countriesArray = await countriesResponse.json();
-      // console.log(countriesArray.Countries);
-      const countries = countriesArray.Countries.map(
-        (country) => country.Country
-      );
-      // console.log(countries);
-      this.setState({
-        countries: countries,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  render() {
-    return (
-      <div className="SearchBar">
-        <Countries
-          countries={this.state.countries}
-          handleCountryChange={this.props.handleCountryChange}
-        />
-      </div>
-    );
-  }
-}
+const SearchBar = ({ countries, handleCountryChange }) => {
+  return (
+    <div className="SearchBar">
+      <Countries
+        countries={countries}
+        handleCountryChange={handleCountryChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
+
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     countries: [],
+//   };
+// }
+// async componentDidMount() {
+//   try {
+//     const countriesResponse = await fetch(
+//       "https://api.covid19api.com/summary "
+//     );
+//     const countriesArray = await countriesResponse.json();
+//     // console.log(countriesArray.Countries);
+//     const countries = countriesArray.Countries.map(
+//       (country) => country.Country
+//     );
+//     // console.log(countries);
+//     this.setState({
+//       countries: countries,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
